b9db17d8d211adecd5646c2fa59421aa672bf23b
mnsh5 <hugo.orlando2050@gmail.com>
2024-05-21 09:46:37 -0300

    implementing an array

--- data-structures/arrays/array_impl.go
+++ data-structures/arrays/array_impl.go
@@ -4,19 +4,46 @@ import "fmt"
 
 type Array[T any] struct {
 	Elements []T
-	Len      int
+	Length   int
 }
 
-// To create a new instance of Array, you can define a function
+// Crea una nueva instancia de un array
 func NewArray[T any](elements []T) *Array[T] {
 	return &Array[T]{
 		Elements: elements,
-		Len:      len(elements),
+		Length:   0,
 	}
+}
+
+func (a *Array[T]) Len() int {
+	l := 0
+	for range a.Elements {
+		l++
+	}
+	return l
+}
+
+func (a *Array[T]) Get(item int) T {
+	return a.Elements[item]
+}
+
+func (a *Array[T]) Append(item T) {
+	a.Elements[a.Length] = item
+	a.Length++
 }
 
 func main() {
 	intArray := NewArray([]int{1, 2, 3, 4, 5})
 	strArray := NewArray([]string{"a", "b", "c"})
 	fmt.Printf("intArray: %d\nstrArray: %s\n", intArray.Elements[2], strArray.Elements[1])
+	fmt.Printf("intArray length: %d\nstrArray length: %d\n", intArray.Len(), strArray.Len())
+
+	intArray2 := NewArray([]int{1, 2, 3, 4, 5})
+	strArray2 := NewArray([]string{"a", "b", "c"})
+	fmt.Printf("intArray: %d\nstrArray: %s\n", intArray2.Get(2), strArray2.Get(1))
+	fmt.Printf("intArray length: %d\nstrArray length: %d\n", intArray2.Len(), strArray2.Len())
+
+	arr := NewArray()
+	arr.Append("Lisboa")
+
 }
