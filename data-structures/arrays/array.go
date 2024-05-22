1ce218d5a646aae9bc0c9eee970060789260d81d
mnsh5 <hugo.orlando2050@gmail.com>
2024-05-22 13:04:30 -0300

    add clear method

--- data-structures/arrays/array.go
+++ data-structures/arrays/array.go
@@ -1,53 +1,54 @@
 package arrays
 
 type Array struct {
-	data   map[int]string
-	length int
+	elements map[int]string
+	size     int
 }
 
 // Crea una nueva instancia de un array
 func NewArray() *Array {
 	return &Array{
-		data:   make(map[int]string),
-		length: 0,
+		elements: make(map[int]string),
+		size:     0,
 	}
 }
 
 func (a *Array) Size() int {
-	l := 0
-	for range a.data {
-		l++
-	}
-	return l
+	return a.size
 }
 
-func (a *Array) Get(item int) string {
-	return a.data[item]
+func (a *Array) Get(index int) string {
+	return a.elements[index]
 }
 
 func (a *Array) Insert(item string) {
-	a.data[a.length] = item
-	a.length++
+	a.elements[a.size] = item
+	a.size++
 }
 
 func (a *Array) GetAll() []string {
 	// value es un slice donde el primer elemento es un array de string,
 	// el cero indica el valor en que iniciamos el slice,
 	// y el len sera el tamaño del slice en base a los datos guardados
-	value := make([]string, 0, len(a.data))
-	for _, v := range a.data {
+	value := make([]string, 0, len(a.elements))
+	for _, v := range a.elements {
 		value = append(value, v)
 	}
 	return value
 }
 
 func (a *Array) Contains(element string) bool {
-	for _, v := range a.data {
+	for _, v := range a.elements {
 		if v == element {
 			return true
 		}
 	}
 	return false
+}
+
+func (a *Array) Clear() {
+	a.elements = map[int]string{}
+	a.size = 0
 }
 
 // func main() {
