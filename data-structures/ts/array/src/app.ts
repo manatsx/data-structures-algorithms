1b9b2a1c743ab75718ccaf4cbf57d843ebc7a703
mnsh5 <hugo.orlando2050@gmail.com>
2024-06-19 22:15:26 +0000

    implementing an array

--- data-structures/ts/array/src/app.ts
+++ data-structures/ts/array/src/app.ts
@@ -4,5 +4,8 @@ const myArray = new MyArray<string>();
 myArray.push("Argentina");
 myArray.push("Bolivia");
 myArray.push("Uruguay");
+myArray.push("Francia");
 console.log(myArray);
 console.log(myArray.get(2));
+// myArray.pop();
+console.log(myArray);
--- data-structures/ts/array/src/array.ts
+++ data-structures/ts/array/src/array.ts
@@ -16,6 +16,16 @@ export class MyArray<T> {
     return this.element[index];
   }
 
+  pop(): T | undefined {
+    if (this.length === 0) {
+      return undefined;
+    }
+    const lastItem = this.element[this.length - 1];
+    this.length--;
+    this.element.length = this.length;
+    return lastItem;
+  }
+
   toString(): string {
     return this.element.slice(0, this.length).toString();
   }
