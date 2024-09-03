d70924ef4d6b48cc961127bbf00496e67cc25a0f
manases <hugo.orlando2050@gmail.com>
2024-09-03 14:07:17 -0300

    oops..

--- csharp/LinkedList/obj/Debug/net8.0/LinkedList.AssemblyInfo.cs
+++ csharp/LinkedList/obj/Debug/net8.0/LinkedList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LinkedList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3925e21b1f78ea2894ac13e5e717adddbdbe423f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0538bfa42b26c5ee6bdbd7ccc48fee84c6460c11")]
 [assembly: System.Reflection.AssemblyProductAttribute("LinkedList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LinkedList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- typescript/hashtable/README.md
+++
@@ -1,3 +0,0 @@
-# Array
-
-run `pnpm start`
