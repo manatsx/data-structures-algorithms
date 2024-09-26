de5a77bf330f73e9e2dc269215492064a7924c2c
manases <hugo.orlando2050@gmail.com>
2024-09-26 09:34:26 -0300

    Doubly LinkedList

--- 04-LinkedList/src/main.ts
+++ 04-LinkedList/src/main.ts
@@ -1,4 +1,5 @@
 import { LinkedList } from "./01-Singly-LinkedList/main";
+import { DoublyLinkedList } from "./02-Doubly-LinkedList/main";
 
 const myLinkedList = new LinkedList<string>("");
 myLinkedList.push("the");
@@ -8,3 +9,12 @@ myLinkedList.push("fox");
 myLinkedList.push("jumps");
 
 console.log(myLinkedList.toString());
+
+const myDoublyLinkedList = new DoublyLinkedList<string>("");
+myDoublyLinkedList.push("the");
+myDoublyLinkedList.push("quick");
+myDoublyLinkedList.push("brown");
+myDoublyLinkedList.push("fox");
+myDoublyLinkedList.push("jumps");
+
+console.log(myDoublyLinkedList.toString());
