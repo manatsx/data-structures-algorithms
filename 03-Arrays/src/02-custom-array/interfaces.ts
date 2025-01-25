1ed15595729170921c1a8496b6288fb830f580ae
manases <hugo.orlando2050@gmail.com>
2025-01-25 15:02:29 -0300

    dsa

--- 03-Arrays/src/02-custom-array/interfaces.ts
+++
@@ -1,9 +0,0 @@
-export interface MyArrayType<T> {
-  length: number;
-  data: { [key: number]: T };
-  push(item: T): number;
-  get(index: number): T | undefined;
-  pop(): T | undefined;
-  shift(): T | undefined;
-  delete(index: number): T | undefined;
-}
